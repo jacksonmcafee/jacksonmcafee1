ada274d0e0f1917be3ef2f79a3e550d5c2d7b06f
jacksonmcafee1 <jacksonmcafee1@gmail.com>
2022-05-03 14:01:09 -0400

    bash: line 1: s: command not found 	modified:   sudoku_solve/main.cpp

--- c++/tools/sudoku_solve/main.cpp
+++ c++/tools/sudoku_solve/main.cpp
@@ -13,7 +13,8 @@ int main() {
   /* TO DO void menu(); */
 
   // allow user to populate board with data
-  while(boardIsLegal() != true) {
+  populateBoard(solve_board);
+  while(boardIsLegal(solve_board) != true) {
     populateBoard(solve_board);
   }
 
@@ -36,7 +37,7 @@ void showBoard(int board[9][9]) {
 void populateBoard(int board[9][9]) {
   for(int i = 0; i < 9; i++) {
     for(int j = 0; j < 9; j++) {
-      solve_board[i][j] = 0;
+      board[i][j] = 0;
     }
   }
   cout << "Input each row 9 characters at a time." << endl;
@@ -59,8 +60,37 @@ void populateBoard(int board[9][9]) {
 // checks if board provided is valid
 bool boardIsLegal(int board[9][9]) {
 
-// check if row is legal
-  for (int i = 0; i < 9; i++) {
-
+// check if board is legal
+  for(int i = 0; i < 9; i++) {
+      for(int j = 0; j < 9; j++) {
+        int temp = board[i][j];
+        if(temp == 0) {
+          goto NEXT;
+        }
+        for(int k = 0; k < 9; k++) {
+          if(temp == board[i][k] && k != j) {
+            // throw error at location [i][j]
+            cout << "Board is invalid. Error at row ";
+            cout << i+1 << ", column " << k+1 << endl;
+            return false;
+          }
+        }
+        for(int k = 0; k < 9; k++) {
+          if(temp == board[k][j] && k != i) {
+            cout << "Board is invalid. Error at row ";
+            cout << k+1 << ", column " << j+1 << endl;
+            return false;
+          }
+        }
+        NEXT:
+        cout << "";
+      }
     }
-  }
+
+  /* TO DO: let user fix board when error is found */
+  /* maybe just cin AT that location and check entire board after */
+  /* else add an iterator array [i, j] and return it as index */
+  /* also need to check that function is efficient */
+
+  return true;
+}
