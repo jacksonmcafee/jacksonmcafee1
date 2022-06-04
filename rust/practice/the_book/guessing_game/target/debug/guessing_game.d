9047cd01b85300c479f6adcb0bad28c9813952d3
jacksonmcafee1 <jacksonmcafee1@gmail.com>
2022-06-04 19:07:14 -0400

    my back hurts a lot

--- rust/practice/the_book/guessing_game/target/debug/guessing_game.d
+++ rust/practice/the_book/guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/home/jacksonm/Documents/personal/rust/practice/guessing_game/target/debug/guessing_game: /home/jacksonm/Documents/personal/rust/practice/guessing_game/src/main.rs
+/home/jacksonm/Documents/personal/rust/practice/the_book/guessing_game/target/debug/guessing_game: /home/jacksonm/Documents/personal/rust/practice/the_book/guessing_game/src/main.rs
