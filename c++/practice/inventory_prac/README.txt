005e412271d2b9b5536df6373548acc0b717c5f1
jacksonmcafee1 <jacksonmcafee1@gmail.com>
2022-06-09 18:16:17 -0400

    new file:   inventory_prac/README.txt 	modified:   inventory_prac/item.h

---
+++ c++/practice/inventory_prac/README.txt
@@ -0,0 +1,6 @@
+Practice to see if I could build a sort of database
+that can be used through the terminal? Mostly practicing using
+maps and file i/o, modifying data types I don't often use and 
+maintaining time efficiency at a larger scale than school
+projects would need me to use.
+
--- c++/practice/inventory_prac/item.h
+++ c++/practice/inventory_prac/item.h
@@ -1,11 +1,3 @@
-/*
- * Practice to see if I could build a sort of database
- * that can be used through the terminal? Mostly practicing using
- * maps and file i/o, modifying data types I don't often use and 
- * maintaining time efficiency at a larger scale than school
- * projects would need me to use 
-*/
-
 #include <map>
 #include <string>
 
