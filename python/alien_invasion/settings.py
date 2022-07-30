9782cac0b675637afa3279be017382e7a6494678
jacksonmcafee1 <jacksonmcafee1@gmail.com>
2022-07-29 20:40:51 -0400

    boomgit add *

--- python/alien_invasion/settings.py
+++ python/alien_invasion/settings.py
@@ -7,3 +7,15 @@ class Settings:
 
         # Ship Settings
         self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 0, 0)
+        self.bullets_allowed = 3
+
+        # Alien Settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1
